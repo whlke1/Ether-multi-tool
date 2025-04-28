491f8e06b14756151f3565694714aeebe029e664
whlke1 <ace.monaro@gmail.com>
2025-04-28 18:19:27 +0100

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,21 @@
+# Copyright (c) Ether (discord.gg/aethergg)
+# See the file 'LICENSE' for copying permission
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
+# EN: 
+#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
+#     - Do not resell this tool, do not credit it to yours.
+
+name_tool       =  "Ether
+type_tool       =  "Multi-Tools"
+version_tool    =  "5.8.9"
+coding_tool     =  "Python 3"
+language_tool   =  "EN"
+creator         =  "app.asar"
+platform        =  "Windows 10/11 & Linux"
+website         =  "discord.gg/aethergg"
+discord_tool     =  "discord.gg/aethergg"
+discord        =  "discord.gg/aethergg"
+url_config      =  "https://raw.githubusercontent.com/loxy0dev/RedTiger-Tools/main/Program/Config/Config.py"
+url_downloads   =  "https://github.com/whlke1/Ether-multi-tool"
+license         =  "https://github.com/whlke1/Ether-multi-tool/blob/Ether/LICENSE"
+copyright       =  "Copyright (c) Ether 'GNU Affero General Public License v3.0'"
